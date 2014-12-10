9c57d3577eef943190e1db88c97b18563aa8c256
SeanGavan <C16Sean.Gavan@usafa.edu>
2014-12-10 12:39:57 -0700

    Code updates

--- Code/main.c
+++ Code/main.c
@@ -6,46 +6,42 @@
 int THRESHOLDLEFT = 0x3B0;		// Threshold voltage for wall prescence
 int THRESHOLDRIGHT = 0x0FF;
 int THRESHOLDCENTER = 0x3D0;
+
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;				  // Stop watchdog timer
     ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; 		  // 64 ADC clks to combat loading, ADC10ON
     ADC10AE0 |= BIT1 + BIT2 + BIT4;           // P1.1, P1.2, P1.4 to ADC mode (A1,A2,A4)
     ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;       // Select SMCLK
+    P1DIR |= BIT0 + BIT6;
 
     for (;;) {
+    	ADC10CTL1 = INCH_4;				// Right receiver A4
+    	ADC10CTL0 |= ENC + ADC10SC;
+    	__bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
+     	if (ADC10MEM > THRESHOLDRIGHT) {
+         	P1OUT |= BIT6;				// P1.6 on
+     	} else {
+     		P1OUT &= ~BIT6;				// P1.6 off
+     	}
+
     	ADC10CTL1 = INCH_1;				// Left receiver A1
-    	P1DIR = BIT0;
-    	P1OUT &= ~BIT0;
     	ADC10CTL0 |= ENC + ADC10SC;     // Sampling and conversion start
     	 __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
     	if (ADC10MEM < THRESHOLDLEFT) {		// Wall close (less V when wall close)
-    		P1OUT |= BIT0;             // P1.0 LED off
+    		P1OUT |= BIT0;             // P1.0 LED on
+    		P1OUT &= ~BIT6;
     	} else {						// Wall close
-    		P1OUT &= ~BIT0;              // P1.0 LED on
+    		P1OUT &= ~BIT0;              // P1.0 LED off
+    		P1OUT &= ~BIT6;
     	}
 
     	ADC10CTL1 = INCH_2;				// Center receiver A2
-    	P1DIR = BIT0;
-    	P1DIR = BIT6;
-    	P1OUT &= ~BIT0;
-    	P1OUT &= ~BIT6;
     	ADC10CTL0 |= ENC + ADC10SC;
     	 __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
     	if (ADC10MEM < THRESHOLDCENTER) {
-    		P1OUT |= BIT0 + BIT6;				// Both off
+    		P1OUT |= BIT0 + BIT6;				// Both on
     	} else {
-    		P1OUT &= ~BIT0 + ~BIT6;				// Both on
-    	}
-
-    	ADC10CTL1 = INCH_4;				// Right receiver A4
-    	P1DIR = BIT6;
-    	P1OUT &= ~BIT6;
-    	ADC10CTL0 |= ENC + ADC10SC;
-    	 __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    	if (ADC10MEM > THRESHOLDRIGHT) {	// Wall close (more V when wall close)
-    		P1OUT |= BIT6;             // P1.3 LED off
-    	} else {
-    		P1OUT &= ~BIT6;              // P1.3 LED on
+    		P1OUT &= ~(BIT0 + BIT6);				// Both off
     	}
    }
 }
